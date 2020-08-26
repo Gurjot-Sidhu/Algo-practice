1400405a7e136db835d1d4d3905b3a65d9072183
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-08-26 14:57:31 -0400

    WIP

---
+++ Practice/firstduplicate.js
@@ -0,0 +1,23 @@
+/*
+Given an array a that contains only numbers in the range from 
+1 to a.length, find the first duplicate number for which the 
+second occurrence has the minimal index. In other words, if 
+there are more than 1 duplicated numbers, return the number 
+for which the second occurrence has a smaller index than the 
+second occurrence of the other number does. If there are no 
+such elements, return -1.
+
+*/
+function firstDuplicate(a) {
+    var found = 0
+    for ( i = 0; i < a.length; i++){
+        for(j = 0; j< a.length; a++){
+            if(i[a] === j[a]){
+                return i[a]
+            }else{
+                return -1
+            }
+        }
+    }
+}
+
