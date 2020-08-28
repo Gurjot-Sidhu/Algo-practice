d5766b6369d9967a937fca39f68518998ba54e45
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-08-28 15:03:48 -0400

    challenge complete

--- Practice/simplarsum.js
+++ Practice/simplarsum.js
@@ -2,3 +2,10 @@
 Return the sum of integers within an array
 */
 
+function simpleArraySum(ar) {
+    var sum = 0
+    for (var i = 0;i < ar.length; i++){
+        sum = sum + ar[i]
+    }
+        return sum
+}
