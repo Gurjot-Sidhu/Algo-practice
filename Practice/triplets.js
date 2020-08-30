1c1b80df7a0cee6712255a97b8e0bc1fcdb95ff2
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-08-29 21:19:54 -0400

    practice

---
+++ Practice/triplets.js
@@ -0,0 +1,5 @@
+/*
+Compare triplets, Compare two arrays with integers. 
+If one of the values is larger than the others, then u add 1 
+and vice versa you thn return the results of the scores for both arrays
+*/
