d628a327da6f6b2e956ba6bce4655ade796d0ca1
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-08-31 14:58:11 -0400

    wip

--- Practice/largesum.js
+++ Practice/largesum.js
@@ -7,6 +7,14 @@ it is a big integer value
 function bigsum (array){
     var sum = 0;
     for(var i = 0;i <array.length;i++){
+        var newstr = array[i]
+        if(newstr.contains("")){
+            var newstr = array[i].toString()
+            var pos = newstr.indexOf(" ")
+            var nwnum = newstr.slice(0,pos).parseInt()
+            sum = sum + nwnum
+        }
         sum = sum + array[i]
     }
+    return sum
 }
