5ba89a1f0ad366e89308dc7f420cf1251f9bdc33
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-08-31 14:34:43 -0400

    wip

---
+++ Practice/largesum.js
@@ -0,0 +1,12 @@
+/* 
+Return the sum of all elements within an array.
+it is a big integer value
+*/
+
+
+function bigsum (array){
+    var sum = 0;
+    for(var i = 0;i <array.length;i++){
+        sum = sum + array[i]
+    }
+}
