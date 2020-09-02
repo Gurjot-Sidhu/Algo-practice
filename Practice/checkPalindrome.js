65c1eeed6ef74aee2ebdf4a68a84d32893f4b4b4
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-02 17:31:45 -0400

    possible solution

--- Practice/checkPalindrome.js
+++ Practice/checkPalindrome.js
@@ -2,3 +2,16 @@
 check if a string is a palindrome
 return true or false
 */
+
+
+
+function palindrome(string){
+    var strlen = string.length()
+        for(var i =0;i <strlen/2;i++){
+            if(string[i] != string[strlen -(i +1){
+                return false
+            }else{
+                return true
+            }
+        }
+}
