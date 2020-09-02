564f60b1e07d9a63716e8ab666fe76367bb46b19
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-02 17:32:49 -0400

    solution

--- Practice/checkPalindrome.js
+++ Practice/checkPalindrome.js
@@ -3,12 +3,10 @@ check if a string is a palindrome
 return true or false
 */
 
-
-
 function palindrome(string){
     var strlen = string.length()
         for(var i =0;i <strlen/2;i++){
-            if(string[i] != string[strlen -(i +1){
+            if(string[i] != string[strlen-(i+1){
                 return false
             }else{
                 return true
