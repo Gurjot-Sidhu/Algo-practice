f21dd695a6dbd48c532441a8e2e6bc69340f5eea
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-04 17:42:02 -0400

    possible solution

--- Practice/adjacentelements.js
+++ Practice/adjacentelements.js
@@ -3,3 +3,16 @@ Given an array of integers,
 find the pair of adjacent elements 
 that has the largest product and return that product
 */
+function adjacentelements(array){
+    var total = 0
+    var a = 0
+    var b = 0
+    for(var i = 0;i < array.length;i++){
+        if(array[i] * array[i+1] > total){
+            total = array[i] * array[i+1]
+            a = array[i]
+            b = array[i+1]
+        }
+    }
+    
+}
