c133aad5a2e956a2b8359b54ebe9e9aa84d0a6b2
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-07 16:26:47 -0400

    possible solution

--- Practice/makearrayconsecutive.js
+++ Practice/makearrayconsecutive.js
@@ -3,3 +3,11 @@ Given an array of integers,determine how many numbers
 would need to be added to make the array contain integers
 that are consecutive
 */
+
+function makeArrayConsecutive(statues){
+    statues.sort(function(a,b){return a - b})
+        var max = statues[statues.length -1]
+        var min = statues[0]
+        var diff = (max-min+1)
+
+    }
