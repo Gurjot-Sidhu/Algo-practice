7ff9b4a948d3f765e17d71126b442dc51f902360
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-09 20:09:12 -0400

    new challenge

--- Practice/twosum.js
+++ Practice/twosum.js
@@ -4,6 +4,11 @@ return indices of the two numbers such that they add up to target
 */
 
 function twoSum(nums,target){
-    for(var i = 0; i < nums.lenth;i++)
-        if()
+    for(var i = 0; i < nums.length;i++){
+        for(var j = i + 1; j < nums.length;j++){
+            if(nums[j] == target - nums[i]){
+                return new int[]
+            }
+        }
+    }
 }
