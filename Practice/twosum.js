c67521e57f6077644f71e7ac7a5dbc8ed328215f
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-09 20:06:23 -0400

    wip

--- Practice/twosum.js
+++ Practice/twosum.js
@@ -1,4 +1,9 @@
 /*
 Given an array of integers nums and an integer target, 
 return indices of the two numbers such that they add up to target
-/*
+*/
+
+function twoSum(nums,target){
+    for(var i = 0; i < nums.lenth;i++)
+        if()
+}
