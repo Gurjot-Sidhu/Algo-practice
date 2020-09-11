9fc1169074030cb1fa2e6bddfcfac9c2a22683d7
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-11 14:19:11 -0400

    possible solution

--- Practice/addtwonumbs.js
+++ Practice/addtwonumbs.js
@@ -4,5 +4,9 @@ return the value of rhe each position added up within another array.
 */
 
 function addTwoNumbers(l1,l2){
-    
+    var newl = []
+    for(var i = 0; i < l1.length; i++){
+        newl[i] = li[i] + l2[i];
+    }
+    return newl
 }
