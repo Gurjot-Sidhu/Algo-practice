5ee42a09f6f8e7bb6aa9f42fdc5fb339a4e0f3d4
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-13 11:50:24 -0400

    new challenge

---
+++ Practice/addtwod.js
@@ -0,0 +1,10 @@
+/* 
+Given one integers return the sum of its digits
+*/
+
+function addTwoDigits(n) {
+    var ndig = n.toString()
+    var dig1 = ndig.slice(0,1)
+    var dig2 = ndig.slice(1,2)
+    return parseInt(dig1 + dig2)
+}
