73b50a57c529477750bd93c49c0d6d430ff243fa
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-13 11:52:37 -0400

    new solution

--- Practice/addtwod.js
+++ Practice/addtwod.js
@@ -5,10 +5,5 @@ possible solution
 
 function addTwoDigits(n) {
 
-    var ndig = n.toString()
-    var dig1 = ndig.slice(0,1)
-    var dig2 = ndig.slice(1,2)
-
-
-    return parseInt(dig1 + dig2)
+   return n % 10
 }
