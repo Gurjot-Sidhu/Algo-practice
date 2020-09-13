30d623a5373d1f668e54de85e145c00ed00232e2
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-13 11:50:55 -0400

    possible solution

--- Practice/addtwod.js
+++ Practice/addtwod.js
@@ -1,10 +1,14 @@
 /* 
 Given one integers return the sum of its digits
+possible solution
 */
 
 function addTwoDigits(n) {
+
     var ndig = n.toString()
     var dig1 = ndig.slice(0,1)
     var dig2 = ndig.slice(1,2)
+
+
     return parseInt(dig1 + dig2)
 }
