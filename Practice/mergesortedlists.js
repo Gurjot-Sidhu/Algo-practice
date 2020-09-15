dea7fbbe79412971f36ee828eb8427a6fd611547
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-15 17:34:48 -0400

    possible solution

--- Practice/mergesortedlists.js
+++ Practice/mergesortedlists.js
@@ -3,8 +3,20 @@ Given 2 sorted lists return the two lists as one new sorted lists
 */
 
 function mergeTwoLists(l1,l2){
-    if(!l1||!l2){}
+    if(!l1||!l2){
         return l1 || l2;
     }
     let l = r;
+    while(l1 && l2){
+        if(l1.val < l2.val){
+            l.val = l1.val
+            l1 = l1.next
+        }else if (l1.val > l2.val){
+            l.val = l2.val
+            l2 = l2.next
+        }else{
+            l.val = l1.val
+            l.next = 
+        }
+    }
 }
