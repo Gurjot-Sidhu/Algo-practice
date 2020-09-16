f03b8503cdbc843c608de0430140e692427a1d1c
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-16 17:39:35 -0400

    solution

--- Practice/implementstrStr.js
+++ Practice/implementstrStr.js
@@ -2,3 +2,16 @@
 Given a two strings, return the index of the 
 first string where the second string exists
 */
+
+function strStr(haystack,needle){
+    var found = -10
+    for(var i = 0;i < 10;i++){
+        found = haystack.search(needle)
+            if(found >= 0){
+                return found
+            }else{
+                return -1
+            }
+        }
+    }
+}
