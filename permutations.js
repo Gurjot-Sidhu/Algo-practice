3d3c803d7a166600139b7762ee8f49e7e820e72f
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-18 17:15:42 -0400

    wip

--- permutations.js
+++ permutations.js
@@ -7,5 +7,6 @@ return all possible permutations without duplicates
 function permute(array){
     for(var i = 0; i < array.length; i++){
         let remnum = array.pop(1)
+        renum = array.length -1 + i
     }
 }
