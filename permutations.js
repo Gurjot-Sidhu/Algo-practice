51bf6c9def648e0881c30bb2c2cd71ebd9f18750
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-18 17:09:28 -0400

    possible solution

--- permutations.js
+++ permutations.js
@@ -2,3 +2,10 @@
 Given a array of different integers, 
 return all possible permutations without duplicates
 */
+
+
+function permute(array){
+    for(var i = 0; i < array.length; i++){
+        let remnum = array.pop(1)
+    }
+}
