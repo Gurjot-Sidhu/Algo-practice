3a8c1ee6d0709a1122b8e9af3418284d61e22404
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-21 19:36:23 -0400

    new challenge

---
+++ Practice/climbingstairs.js
@@ -0,0 +1,9 @@
+/* 
+Given a staricase, where u can climb one step or 2 steps at a time, 
+Calculate how many distinct waus you can climb to the top of the staircase.
+*/
+
+function climbingStairs = n =>{
+    a = [0,1,2]
+
+}
