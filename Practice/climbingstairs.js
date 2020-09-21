87208abf8fa0b0a16ba10eddc9cbf00a490ae1e0
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-21 19:38:44 -0400

    wip

--- Practice/climbingstairs.js
+++ Practice/climbingstairs.js
@@ -6,4 +6,8 @@ Calculate how many distinct waus you can climb to the top of the staircase.
 function climbingStairs = n =>{
     a = [0,1,2]
 
+    for(i = 2; i++ <= n;){
+        a[i] = a[i - 1] + a[i -2]
+    }
+    
 }
