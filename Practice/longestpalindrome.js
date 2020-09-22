ac1f227a2f032d4311c03fa124c82e0c27e5cd83
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-22 19:58:08 -0400

    solution

--- Practice/longestpalindrome.js
+++ Practice/longestpalindrome.js
@@ -9,7 +9,15 @@ function longestPalindrome(s){
     let [max_l,offset] = [0,1];
     for(var i = 0;i < s.length-1;i++){
         let [l,r]= [i,i];
-        while(l >= 0 && r < s.length -1 && s[l] === s[r+1]) 
-        
+        while(l >= 0 && r < s.length -1 && s[l] === s[r+1]){
+            r +=1;
+        }
+        while (l >= 0 && r<s.length && s[l] === s[r]){
+            if(r + 1 - l > offset){
+                [max_l,offset] = [l,,r+1 - l]
+                l -= 1; r+=1
+            }
+        }
+        return s.substring(max_l,max_l + offset)
     }
 }
