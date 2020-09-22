cba6aec8154f6ac4a57dbacd9046cc51207e3064
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-22 19:51:48 -0400

    wip

--- Practice/longestpalindrome.js
+++ Practice/longestpalindrome.js
@@ -1,3 +1,10 @@
 /* 
 Given a long string, Find the longest palindrome within the string 
 */
+
+function longestPalindrome(string){
+    if (s === null || s.length < 1){
+        return console.log("Empty")
+    }
+    for(var i = 0;)
+}
