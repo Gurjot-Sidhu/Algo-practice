5e3237bdbd7c6fd0fdc2eac853125c42f0159af2
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-22 19:55:20 -0400

    possible solution

--- Practice/longestpalindrome.js
+++ Practice/longestpalindrome.js
@@ -2,9 +2,14 @@
 Given a long string, Find the longest palindrome within the string 
 */
 
-function longestPalindrome(string){
+function longestPalindrome(s){
     if (s === null || s.length < 1){
         return console.log("Empty")
     }
-    for(var i = 0;)
+    let [max_l,offset] = [0,1];
+    for(var i = 0;i < s.length-1;i++){
+        let [l,r]= [i,i];
+        while(l >= 0 && r < s.length -1 && s[l] === s[r+1]) 
+        
+    }
 }
