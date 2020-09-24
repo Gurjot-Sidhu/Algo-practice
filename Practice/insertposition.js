24e7d391367fb0d1e3157acd738a8802760a96d4
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-23 22:37:51 -0400

    wip

--- Practice/insertposition.js
+++ Practice/insertposition.js
@@ -2,3 +2,15 @@
 Given a sorted array and target value, 
 return the index in a number could be inserted in order. No duplicates
 */
+
+function position(nums,target){
+    for(var i = 0; i <nums.length;i++){
+        if(nums[i] < target && nums[i+1] > target){
+            return i+1
+        }else if(nums[i] === target){
+            return 0
+        }else{
+            return 0 
+        }
+    }
+}
