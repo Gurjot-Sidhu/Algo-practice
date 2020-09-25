6f2e67baecf65e52e9edadfcc7e06407a4bd98ea
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-25 16:30:21 -0400

    new challenge

---
+++ Practice/houserobber.js
@@ -0,0 +1,9 @@
+/* 
+Given a number of houses witha security sytem that activates when two 
+adjoinging houses are robbed, how many houses could be robbed without 
+triggering that alarm
+*/
+
+function houseRobber(nums){
+    for
+}
