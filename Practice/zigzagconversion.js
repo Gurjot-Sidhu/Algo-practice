593a5a40eba41646c851a2e4713da34d52469295
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-28 15:30:13 -0400

    wip

--- Practice/zigzagconversion.js
+++ Practice/zigzagconversion.js
@@ -2,6 +2,8 @@
 Given a row of string convert the zigzag pattern to a string
 */
 
-function zigZag (){
-    
+function zigZag(s,numrows){
+    for(var i = 0;i <numrows.length;i++){
+        
+    }
 }
