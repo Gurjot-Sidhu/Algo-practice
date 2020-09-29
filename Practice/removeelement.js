8dfeeeff2e1b513a1160477f64284f3e404605cd
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-29 17:03:04 -0400

    newchallenge

---
+++ Practice/removeelement.js
@@ -0,0 +1,10 @@
+/* 
+Given an array and a value remove alll instance of that 
+value and return the new length
+*/
+
+
+function removeElement(nums,value){
+    var filtered = array.filter(function(val,index,nums){return val != value})
+    return filtered.length;
+}
