466e18d430137bd807d4ea6aacbacbf2525d3500
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-30 17:59:36 -0400

    wip

--- Practice/decode/interview.js
+++ Practice/decode/interview.js
@@ -5,6 +5,18 @@ Given a message containing letters from A-Z decode a non-empty
 string and determine thte number of total ways to decode it
 */
 
-function decodeways(){
-
+function decodeways(s){
+    let f = []
+    f[-1] = 1;
+    for (var i = 0; i < s.length; i++){
+        f[i] = 0;
+        if(s[i] !== "0"){
+            f[i] += f[i-1]
+        }
+        let twodigitNum = (i > 0) ? (parseInt(s.substr(i-1),2,10)) : 0
+        if (twodigitNum >=10 && twodigitNum <= 26){
+            f[i] += f[i-2];
+        }
+    }
+    re
 }
