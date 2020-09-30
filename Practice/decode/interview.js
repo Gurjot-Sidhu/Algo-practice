002a2c9e33d12ee2ccd21763c74aded7361875d3
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-09-30 17:55:25 -0400

    more background

--- Practice/decode/interview.js
+++ Practice/decode/interview.js
@@ -1,4 +1,10 @@
 /* 
 I was given almost the exact same problem for a JPMorgan and Co. interview
 I am doing this to restudy the question and find a new solution
+Given a message containing letters from A-Z decode a non-empty 
+string and determine thte number of total ways to decode it
 */
+
+function decodeways(){
+
+}
