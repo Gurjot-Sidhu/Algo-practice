f2ea452ef9573fe1afd53e8edab29945472a3123
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-10-01 16:26:47 -0400

    wip

--- Practice/traprainwater.js
+++ Practice/traprainwater.js
@@ -2,3 +2,12 @@
 Given n non negative integeres representing an elevation map where the
  width of each bar is 1,compute how much water it is able to trap after raining
 */
+
+function rainWater(height){
+    var counter = 0;
+    for(var i = 0;i<height.length;i++){
+        if(height[i] === 1 && height[i-1] === 0 && height[i+1] > 0){
+            var leftheight =
+        }
+    }
+}
