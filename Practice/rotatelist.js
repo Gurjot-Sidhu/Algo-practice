2b56dfea679c8c11b0f9eabcb05c1c9d5f595140
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-10-07 15:08:02 -0400

    wip

--- Practice/rotatelist.js
+++ Practice/rotatelist.js
@@ -4,5 +4,15 @@ non negative without making the array larger
 */
 
 function rotateList(head,k){
+    var len = head.length
     
+    for(var i = 0;i<head.length;i++){
+        var newpos;
+        if(i+k > len){
+            head[i] = head[(i+k)% len]
+        }else if(i+k < len){
+            head[i] = head[i+k]
+            
+        }
+    }
 }
