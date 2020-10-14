6de31626162b9dd244f9486a91bd54667ec51be8
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-10-14 19:01:32 -0400

    wip

--- Practice/house robber.js
+++ Practice/house robber.js
@@ -3,3 +3,14 @@ Houses are arranged in a cricle where there is an alarm system
 that activates when two adjacent houses are robbed
 how many houses can you rob without tripping the alarm
 */
+
+function robber(s){
+    let total;
+    for(var i = 0;i<s.length;i++){
+        if(s.length % 2 === 0){
+            total = total + s[i]
+        }else{
+            total =
+        }
+    }
+}
