b4334b583cb85be37f07fe60f7a14475e0f64244
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-10-16 19:03:20 -0400

    possible solution

--- Practice/search2Dmatrix.js
+++ Practice/search2Dmatrix.js
@@ -3,9 +3,14 @@ Given a m x n matrix that contains integers which are in order.
 Find weather or not a value exists within the matrix
 */
 
-function 2d(matrix,target){
+function twodsearch(matrix,target){
     for(var i = 0;i<matrix.length;i++){
-        for(j = 0;j<matrix[i].length;j++{
-            if()
+        for(j = 0;j<matrix[i].length;j++){
+            if(matrix[i][j].includes(target)){
+                return true
+            }else{
+                return false
+            }
+        }
     }
 }
