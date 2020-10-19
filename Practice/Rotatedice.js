b041cc6492a46c34f87311aa5c545a228b22aff4
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-10-19 17:02:25 -0400

    wip

--- Practice/Rotatedice.js
+++ Practice/Rotatedice.js
@@ -5,5 +5,19 @@ Return the minimum number of rotations
 */
 
 function minDominoRotations(A,B){
-    
+    var mostfreq= 1;
+    var m = 0;
+    var item;
+    for(var i=0;i<A.length;i++){
+        for(var j=i;j<A.length;j++){
+            if(A[i] === A[j]){
+                m++;
+            }
+            if(mostfreq<m){
+                mostfreq=m;
+                item = arr[i];
+            }
+        }
+    }
+    m=0
 }
