4d6b262e93a4f8a754d94b920f76f972726c1b07
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-10-19 16:52:23 -0400

    new challenge

---
+++ Practice/Rotatedice.js
@@ -0,0 +1,9 @@
+/* 
+Given two rows of dice that are opposistes of each other 
+rotate the positions of the row so one of the rows are all the same.
+Return the minimum number of rotations 
+*/
+
+function minDominoRotations(A,B){
+    
+}
