48141503cadf07fb84ba027601df1bb54da1ba1f
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-10-21 18:19:15 -0400

    new challenge

---
+++ Practice/asteroidcollision.js
@@ -0,0 +1,11 @@
+/* 
+Given an array of integers return the final result of the asteroid collisions.
+Equal size integers cancel eachother out. The larger deletes the smaller.
+Negative integers move left and positive move right.
+*/
+
+function asteroid(list){
+    for(var i=0;i<list.length;i++){
+        
+    }
+}
