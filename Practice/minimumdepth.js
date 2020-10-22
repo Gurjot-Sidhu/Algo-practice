d0536c00ccec49c58327284b7ff962b8f771692e
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-10-22 16:19:22 -0400

    wip

--- Practice/minimumdepth.js
+++ Practice/minimumdepth.js
@@ -3,5 +3,8 @@ Given a binary tree, determine the minimum depth within the tree
 */
 
 function mindepth(root){
-    
+    var mincounter = 0;
+    var currentc = 0
+    for(var i = 0; i<root.length;i++){
+    }
 }
