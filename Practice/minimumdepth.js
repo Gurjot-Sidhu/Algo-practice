db69b1caf3396e44b1f46cb3069e1c861db41545
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-10-27 17:26:44 -0400

    wip

--- Practice/minimumdepth.js
+++ Practice/minimumdepth.js
@@ -6,9 +6,10 @@ function mindepth(root){
     var mincounter = 0;
     var currentc = 0
     for(var i = 0; i<root.length;i++){
-        if(isNaN(root[i]) === false && currentc = 0){
+        if(isNaN(root[i]) === false && currentc === 0){
             currentc = 0
-            
+        }else if(isNaN(root[i]=== true && currentc === 0)){
+
         }
     }
 }
