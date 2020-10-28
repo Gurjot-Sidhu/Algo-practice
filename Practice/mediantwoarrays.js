eeaff0a52c1f21d3863e3cfd61bd56b33b27d419
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-10-28 19:37:29 -0400

    sort added

--- Practice/mediantwoarrays.js
+++ Practice/mediantwoarrays.js
@@ -2,4 +2,8 @@
 Given two sorted arrays return the median of two arrays
 */
 
-function
+function sortedmedian(nums1,nums2){
+    var nums3 = nums1.concat(nums2);
+    var median ;
+    nums3.sort(function(a,b){return a - b})
+}
