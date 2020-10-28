8437d575bd07ebb10727abd1b254ee6f204aa295
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-10-28 19:38:17 -0400

    if statement added

--- Practice/mediantwoarrays.js
+++ Practice/mediantwoarrays.js
@@ -6,4 +6,11 @@ function sortedmedian(nums1,nums2){
     var nums3 = nums1.concat(nums2);
     var median ;
     nums3.sort(function(a,b){return a - b})
+    if(nums3.length % 2 > 0){
+        median = (nums3[(nums3.length/2)] + nums3[(nums3.length/2 + 1)]) /2
+        return median;
+    }else{
+         
+    }
+
 }
