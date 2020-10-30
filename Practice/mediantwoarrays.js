5637567b7e44d339dbca5f221b87080ecba59de1
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-10-29 21:06:42 -0400

    messing around with test code

--- Practice/mediantwoarrays.js
+++ Practice/mediantwoarrays.js
@@ -1,14 +1,16 @@
 /* 
 Given two sorted arrays return the median of two arrays
 */
+var nums1 = [1,3];
+var nums2 = [2];
 
 function sortedmedian(nums1,nums2){
     var nums3 = nums1.concat(nums2);
     var median ;
     nums3.sort(function(a,b){return a - b})
     if(nums3.length % 2 > 0){
-        median = (nums3[(nums3.length/2)] + nums3[(nums3.length/2 + 1)]) /2
-        return median;
+        median = (nums3[(Math.floor(nums3.length/2))] + nums3[(Math.floor(nums3.length/2 + 1))]) /2
+        console.log(median)
     }else{
          
     }
