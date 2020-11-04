fccaf832c2edbee099513208074189fcef88ee5a
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-11-04 16:40:17 -0500

    testing

--- Practice/uniquepermutations.js
+++ Practice/uniquepermutations.js
@@ -6,17 +6,18 @@ return all possible unique permutations in any order
 function perm (nums){
     var results;
     if(nums.length = 1){
-        results = nums
+        results = nums;
         return results
     }
     for(var i = 0;i < nums.length;i++){
-        var first = nums[0]
-        var leftover = nums.splice(1)
+        var first = nums[0];
+        var leftover = nums.splice(1);
         var innerperm = perm(leftover);
         for(var j = 0;j < innerperm.length;j++){
-            results.push(first + innerperm[j])
+            results.push(first + innerperm[j]);
         }
 
     }
+    console.log(results)
     return results
 }
