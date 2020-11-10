ccb6034e5f9ff047f2669c3295098eb09053fd1c
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-11-10 14:46:31 -0500

    wip

--- Practice/maximumsubarray.js
+++ Practice/maximumsubarray.js
@@ -4,5 +4,11 @@ find the contigous subarray which has the largest sum and return its sum
 */
 
 function maximumsubarray(nums){
-    
+    for(var i = 0;i <nums.length;i++){
+        var size = [];
+        var sum = 0 
+        if(nums[i] + nums[i+1] > sum){
+            sum += nums[i] + nums[i+1]
+        }
+    }
 }
