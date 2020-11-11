48a85f163a5e88795245d69acd776c7c5e75f8b0
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-11-11 18:19:15 -0500

    new else if

--- Practice/maximumsubarray.js
+++ Practice/maximumsubarray.js
@@ -5,10 +5,12 @@ find the contigous subarray which has the largest sum and return its sum
 
 function maximumsubarray(nums){
     for(var i = 0;i <nums.length;i++){
-        var size = [];
         var sum = 0 
         if(nums[i] + nums[i+1] > sum){
             sum += nums[i] + nums[i+1]
+        }else if{
+            nums[i] + nums[i+1] + nums[i+2] > sum
+            sum = nums[i] + nums[i+1] + nums[i+2] 
         }
     }
 }
