afcb6bae06d4600718007082d4951b25b58da049
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-11-13 18:04:29 -0500

    wip

--- Practice/longestcommonprefix.js
+++ Practice/longestcommonprefix.js
@@ -3,7 +3,9 @@ Given an array of strings return the longest common prefix
 */
 
 function prefix(words){
+    var common = ""
     for(var i = 0;i<words.length;i++){
-        
+
     }
+    return ""
 }
