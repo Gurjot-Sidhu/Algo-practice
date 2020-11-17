97a7fc403903406479c629c6614eaa8b1c591f73
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-11-17 14:36:10 -0500

    wip

--- Practice/reverseint.js
+++ Practice/reverseint.js
@@ -2,3 +2,10 @@
 Given an 32 bit integer reverse the digits of said integer
 */
 
+function reverse(int){
+    if(int === 0){
+        return 0
+    }else{
+        int.toString(),split('').reverse().join('')
+    }
+}
