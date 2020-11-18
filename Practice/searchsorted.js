adb65018478489680d76a080acdc7d79c30bc3c1
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-11-18 16:00:05 -0500

    new challenge

---
+++ Practice/searchsorted.js
@@ -0,0 +1,11 @@
+/* 
+Given a sorted array that may be rotated, find the target
+*/
+
+function search(nums,target){
+    var foundpos;
+    if(nums.indexOf(target) === 0){
+        foundpos = -1
+    }
+   
+}
