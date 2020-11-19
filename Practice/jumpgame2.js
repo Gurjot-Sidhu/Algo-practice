b2571219002c54ab40923df281168d34fcd14a9d
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-11-19 16:42:43 -0500

    wip

--- Practice/jumpgame2.js
+++ Practice/jumpgame2.js
@@ -3,3 +3,13 @@ Given an array of non-negative integeers nums, you start at the first position
 and its value is how many places you move in the array, determine the least 
 amount of jumps needed to reach the end.
 */
+
+function jump(nums){
+    var jumps = 0;
+    for(var i = 0;i<nums.length;i++){
+        if(nums[0] + nums[i] === nums.length){
+            jumps = 2
+        }
+    }
+    return jumps
+}
