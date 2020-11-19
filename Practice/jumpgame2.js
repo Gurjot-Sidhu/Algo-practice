df4cab9afd3c16de27069fbd37b6cb2b8fa51af1
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-11-19 16:40:10 -0500

    new challenge

---
+++ Practice/jumpgame2.js
@@ -0,0 +1,5 @@
+/* 
+Given an array of non-negative integeers nums, you start at the first position 
+and its value is how many places you move in the array, determine the least 
+amount of jumps needed to reach the end.
+*/
