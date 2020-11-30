9639f4fe7cb2e7086bf18d9a7fee6d07ebc3238e
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-11-30 15:02:01 -0500

    new solution

--- Practice/removeduplicate.js
+++ Practice/removeduplicate.js
@@ -3,5 +3,12 @@ Given an array of sorted nums remove all of the duplicates and return the length
 */
 
 function removeDuplicates(nums){
-    for(var )
+    for(var i = 0;i<nums.length;i++){
+        if(i+1 <=nums.length){
+            if(nums[i] === nums[i+1]){
+                nums.splice(i,1)
+            }
+        }
+    }
+    return nums.length
 }
