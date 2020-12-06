3e95143943e1c189ae6f703078ed380d0b0785a2
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-12-06 17:39:36 -0500

    wip

--- Practice/firstmissingpositive.js
+++ Practice/firstmissingpositive.js
@@ -3,7 +3,12 @@ Given an unsorted array find the smallest possible positive integer
 */
 
 function firstMissingPositive(nums){
+    var target = 1
     for(var i = 0;i<nums.length;i++){
-        if(nums[i] )
+        if(nums.includes(target) === true){
+            target++
+        }else{
+            return target
+        }
     }
 }
