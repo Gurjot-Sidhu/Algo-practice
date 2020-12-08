93f7f01b726e23b973f221e1dcd87c025900690e
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-12-08 15:26:44 -0500

    new challenge

---
+++ Practice/subswithconcatofall.js
@@ -0,0 +1,9 @@
+/* 
+Given a string s and and an array of strings, 
+return all starting indices of substrings in s that is a 
+concatenation of each word.
+*/
+
+function findSubs(s,words){
+    
+}
