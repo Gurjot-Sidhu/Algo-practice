18a19f72a05a99fbbab9489257488b353a3f62f7
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-12-11 17:31:07 -0500

    wip

--- Practice/atoi.js
+++ Practice/atoi.js
@@ -4,5 +4,11 @@ in it return what it consists of including the number
 */
 
 function atoi(s){
-    
+    if(s){
+        if(parseInt(s,10)){
+            return parseInt(s,10)
+        }
+    }else{
+        return 0
+    }
 }
