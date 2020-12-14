1bffbde7bc5fc8a0bd686a5fabd8fb6d3b295e1e
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-12-14 16:51:19 -0500

    wip

--- Practice/plusone.js
+++ Practice/plusone.js
@@ -3,7 +3,7 @@ Given a non-empty array of decimals add on to the value
 */
 
 function plus0ne(digits){
-    var lastpos = digts.length - 1;
+    var lastpos = digits.length - 1;
     if(digits[lastpos] === 9){
         digits[lastpos] == 1
         digits.push(0)
